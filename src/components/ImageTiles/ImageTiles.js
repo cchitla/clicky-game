bfb569ddb325fd021bac70a474dbcbc690d90572
Chris Chitla <chrischitla@gmail.com>
2020-01-14 21:54:27 -0700

    function to handle click on selected image

--- src/components/ImageTiles/ImageTiles.js
+++ src/components/ImageTiles/ImageTiles.js
@@ -6,7 +6,7 @@ import Card from 'react-bootstrap/Card';
 const ImageTiles = (props) => {
   return (
     <>
-      <Card style={{ width: '18rem' }}>
+      <Card onClick={e => props.handleClick(e, props.isSelected, props.id)} style={{ width: '18rem' }}>
         <Card.Img variant="top" src={props.img} />
       </Card>
     </>
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -5,38 +5,40 @@ import ImageTiles from '../ImageTiles/ImageTiles';
 
 import CardColumns from 'react-bootstrap/CardColumns';
 
-import Characters from '../../matchChars.json'
+import Characters from '../../matchChars.json';
 
 // all state goes here
 const Wrapper = () => {
   const [score, setScore] = useState(0);
   const [wins, setWins] = useState(0);
 
+  const randomizeArray = (Characters) => {
+    //return randomized array
+  } 
+
   const setTiles = () => {
-    //randomize array and return 
+    randomizeArray(Characters)
     return (
       <>
-        <ImageTiles handleClick={handleClick} img={Characters[0].img} />
-        <ImageTiles handleClick={handleClick} img={Characters[1].img} />
-        <ImageTiles handleClick={handleClick} img={Characters[2].img} />
+        <ImageTiles handleClick={handleClick} img={Characters[0].img} id={Characters[0].id} isSelected={Characters[0].isSelected}/>
+        <ImageTiles handleClick={handleClick} img={Characters[1].img} id={Characters[1].id} isSelected={Characters[1].isSelected}/>
+        <ImageTiles handleClick={handleClick} img={Characters[2].img} id={Characters[2].id} isSelected={Characters[2].isSelected}/>
       </>
-    )
-  }
+    );
+  };
 
   const resetGame = () => {
     //setScore(0)
+    //setTiles()
+  };
 
-
-  }
-
-  const handleClick = () => {
-    // if selected !== true
-    setScore(score + 1)
-    //
-
-    // else if selected === true 
-    // resetGame()
-
+  const handleClick = (e, isSelected, id) => {
+    if (isSelected === false) {
+      setScore(score + 1)
+    };
+    const arrIndex = Characters.findIndex(item => item.id === id);
+    
+    Characters[arrIndex].isSelected = true;
   };
 
   return (
